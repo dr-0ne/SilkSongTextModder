0adfca1bf6a914baf668284a501446d5ef2edf25
TinyDerp <pumpanddumpschemer@gmail.com>
2026-01-03 00:06:27 +0200

    Initial chnage
    Fixed bug from original and added code for Thunderstore addon mod support.

--- README.md
+++ README.md
@@ -1,43 +1,51 @@
-# Silksong Text Modder
+## Silksong Text Modder
 
 A mod that allows editing, overriding, and creation of new text items of Silksong's internal data. This can be used to change dialogue, names, tutorial text, menu items, etc.
 
-## How do I Create new Text?
+[Original Mod]()
+[Source Code](https://github.com/KyleNeubarth/SilkSongTextModder)  
+[Text Asset Decrypter](https://github.com/KyleNeubarth/SilkSongTextAssetDecrypter) (Simple program to help you view Silksong's ingame text)
 
-This mod reads text in files located in `\Hollow Knight Silksong\Hollow Knight Silksong_Data\Mods\TextModder`. These files have a precise format where you specify the localization language code, Sheet ID, Entry ID, and finally text value. All values are seperated by ">". I tried to use XML, JSON parsing but bepinex/harmony would crash during that for some reason. If enough people complain I will likely change this.
+### How do I Modify Text?
 
-
-For example, a file with this line of text will override the main menu's quit button text (normally "Quit Game") with "Give Up". Note that this will only apply to the "EN" language code. You could put this line in `\Hollow Knight Silksong\Hollow Knight Silksong_Data\Mods\TextModder\giveuptest.txt` and it will apply the next time you start the game.
-```
-EN>MainMenu>MAIN_QUIT>Give Up
-```
-
-In order to figure out which "sheet" and "entry" the text you want to modify lives, you must inspect the existing game's TextAssets. See further instructions below.
-
-It is also possible to add new sheets and entries which the vanilla game does not use. You may be able to use these for modded content, but I haven't done any of that. Good luck!
+This mod reads text in files located in **"\Hollow Knight Silksong\Hollow Knight Silksong_Data\Mods\TextModder"**.  
+These files have a precise format where you specify the localization language code, Sheet ID, Entry ID, and finally text value. All values are seperated by ">". I tried to use XML, JSON parsing but bepinex/harmony would crash during that for some reason. If enough people complain I will likely change this.
 
 
-## How do I extract existing text from the game for reference?
-Internally, Silksong keeps localized text in large XML tables called "sheets". Sheets are arbitrary groupings of text that the developers thought was helpful for organization (EN_Song contains all the text for needolin NPC sing along music, for example).These sheets are TextAssets stored in the resources assetbundle (`\Hollow Knight Silksong\Hollow Knight Silksong_Data\resource.assets`). I used [AssetRipper](https://github.com/AssetRipper/AssetRipper) to decompile the AssetBundle and view the contents. 
+For example, a file with this line of text will override the main menu's quit button text (normally "Quit Game") with "Give Up". Note that this will only apply to the "EN" language code. You could put this line in **"\Hollow Knight Silksong\Hollow Knight Silksong_Data\Mods\TextModder\giveuptest.txt"** and it will apply the next time you start the game.  
+`EN>MainMenu>MAIN_QUIT>Give Up`
 
-Note: If using AssetRipper remember to specify the Unity Version in it's settings. Silksong is using Unity `6000.0.50f1`, which can be found by looking at the properties of the executable.
+In order to figure out which "sheet" and "entry" the text you want to modify lives, you must inspect the existing game's TextAssets. See further instructions below or check out the [DecryptedTextSheets](https://www.nexusmods.com/hollowknightsilksong/mods/381?tab=files) from the original.
 
-Sheets have some laughable code obfuscation function which they are run through. Via decompiling Silksong's `Assembly.CSharp.dll` with [Dnspy](https://github.com/dnSpy/dnSpy) I found that function which is pasted below. I intend on making some tool to automatically do this in the future. In the meantime this project has a folder "RawText" which contains the decrypted sheets, but it's possible these sheets will be out of date when you read them in the future.
+It is also possible to add new sheets and entries which the vanilla game does not use. You may be able to use these for modded content, but I haven't been able to test any of that. Good luck!
 
-```
-public static byte[] Decrypt(byte[] encryptedBytes)
-    {
-        byte[] result;
-        using (RijndaelManaged rijndaelManaged = new RijndaelManaged
-               {
-                   Key = _keyArray,
-                   Mode = CipherMode.ECB,
-                   Padding = PaddingMode.PKCS7
-               })
-        {
-            result = rijndaelManaged.CreateDecryptor().TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
-        }
-        return result;
-    }
-private static readonly byte[] _keyArray = Encoding.UTF8.GetBytes("UKu52ePUBwetZ9wNX88o54dnfKRu0T1l");
+### How to I make a mod using this?
+Create the text file like you would do normally for this mod. Then create the mod, place the text file in the mod folder (same folder where your `manifest.json` file is located)  
+And lastly add a file into the same folder and call it "`TextModder`". This file has to be exactly that. No lower case letters no *.txt*'s, just "`TextModder`".
+
+### How do I extract existing text from the game for reference?
+
+**TLDR:** I have extracted and pinned a snapshot of the current English text sheets to this mod's files. Otherwise you will need to extract the data yourself.
+
+Internally, Silksong keeps localized text in large XML tables called "sheets". Sheets are arbitrary groupings of text that the developers thought was helpful for organization (EN_Song contains all the text for needolin NPC sing along music, for example). These sheets are TextAssets stored in the resources assetbundle (**"\Hollow Knight Silksong\Hollow Knight Silksong_Data\resource.assets"**). I used [AssetRipper](http://https//github.com/AssetRipper/AssetRipper) to decompile the AssetBundle and view the contents.
+
+**Note:** If using AssetRipper remember to specify the Unity Version in it's settings. Silksong is using Unity "6000.0.50f1", which can be found by looking at the properties of the executable.
+
+Sheets have some laughable code obfuscation function which they are run through. Via decompiling Silksong's "Assembly.CSharp.dll" with [Dnspy](http://https//github.com/dnSpy/dnSpy) I found the obfuscation function which is pasted below.
+```csharp
+Decryption Functionpublic static byte[] Decrypt(byte[] encryptedBytes)
+{
+  byte[] result;
+  using (RijndaelManaged rijndaelManaged = new RijndaelManaged
+{
+ Key = _keyArray,
+ Mode = CipherMode.ECB,
+ Padding = PaddingMode.PKCS7
+})
+  {
+result = rijndaelManaged.CreateDecryptor().TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+  }
+  return result;
+}
+private static readonly byte[] _keyArray = Encoding.UTF8.GetBytes("UKu52ePUBwetZ9wNX88o54dnfKRu0T1l"); 
 ```
--- SilkSongTextModder.cs
+++ SilkSongTextModder.cs
@@ -1,12 +1,10 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Xml;
-using System.Xml.Linq;
-using BepInEx;
-using BepInEx.Logging;
-using HarmonyLib;
 using TeamCherry.Localization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -20,11 +18,11 @@ public class TextModderPlugin : BaseUnityPlugin
     private static readonly string ModDir = Path.Combine(Application.dataPath, "Mods", "TextModder");
 
     private static bool _patched;
-    
+
     private void Awake()
     {
         if (!Directory.Exists(ModDir)) Directory.CreateDirectory(ModDir);
-        
+
         BepInEx.Logging.Logger.Sources.Add(TextModderLogger);
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
@@ -42,30 +40,30 @@ public class TextModderPlugin : BaseUnityPlugin
         _patched = true;
         TextModderLogger.LogInfo("Harmony patching complete");
     }
-    
+
     [HarmonyPatch]
     class Patch_Language_DoSwitch
     {
-        
+
         static MethodBase TargetMethod()
         {
             return AccessTools.Method(typeof(Language), "DoSwitch", [typeof(LanguageCode)]);
         }
-        
+
         static void Postfix()
         {
             // Language -> sheet -> entryId -> value
             // A little gross I'm aware
             Dictionary<LanguageCode, Dictionary<string, Dictionary<string, string>>> customSheetsByLanguage = ReadInCustomText();
-            
+
             FieldInfo? currentEntrySheetsFieldInfo = typeof(Language).GetField("_currentEntrySheets", BindingFlags.NonPublic | BindingFlags.Static);
-            
+
             if (currentEntrySheetsFieldInfo == null)
             {
                 TextModderLogger.LogError("Failed to find '_currentEntrySheets' field");
                 return;
             }
-            
+
             Dictionary<string, Dictionary<string, string>>? currentEntrySheets = currentEntrySheetsFieldInfo?.GetValue(null) as Dictionary<string, Dictionary<string, string>>;
 
             if (currentEntrySheets == null)
@@ -75,21 +73,21 @@ public class TextModderPlugin : BaseUnityPlugin
             }
 
             FieldInfo? currentLanguageFieldInfo = typeof(Language).GetField("_currentLanguage", BindingFlags.NonPublic | BindingFlags.Static);
-            
+
             if (currentLanguageFieldInfo == null)
             {
                 TextModderLogger.LogError("Failed to find '_currentLanguage' field");
                 return;
             }
-            
+
             LanguageCode currentLanguage = currentLanguageFieldInfo.GetValue(null) is LanguageCode ? (LanguageCode)currentLanguageFieldInfo.GetValue(null) : LanguageCode.N;
 
             if (!customSheetsByLanguage.ContainsKey(currentLanguage))
             {
-                TextModderLogger.LogWarning("There is no custom text for the game's current language code: '" + currentLanguage +"'");
+                TextModderLogger.LogWarning("There is no custom text for the game's current language code: '" + currentLanguage + "'");
                 return;
             }
-            
+
             // Copy each custom value into the game's text data
             foreach (var sheet in customSheetsByLanguage[currentLanguage])
             {
@@ -102,30 +100,59 @@ public class TextModderPlugin : BaseUnityPlugin
                     currentEntrySheets[sheet.Key][entry.Key] = entry.Value;
                 }
             }
-            
-            currentLanguageFieldInfo.SetValue(null,currentEntrySheets);
-            
+
+            currentEntrySheetsFieldInfo.SetValue(null, currentEntrySheets);
+
             TextModderLogger.LogInfo("Replaced all text entries");
         }
 
         static Dictionary<LanguageCode, Dictionary<string, Dictionary<string, string>>> ReadInCustomText()
         {
+            // Read from bepinex plugin dirs first
             var returnDict = new Dictionary<LanguageCode, Dictionary<string, Dictionary<string, string>>>();
-            foreach (var filePath in Directory.GetFiles(ModDir))
+            string pluginRoot = BepInEx.Paths.PluginPath;
+            if (Directory.Exists(pluginRoot))
             {
-                TextModderLogger.LogInfo($"Reading Text From File: {filePath}");
-                foreach (var line in File.ReadAllLines(filePath))
+                TextModderLogger.LogDebug($"BepInEx plugin directory found at: {pluginRoot}");
+                TextModderLogger.LogDebug("PluginRoot (recursive):\n" +
+                    string.Join("\n", Directory.EnumerateFileSystemEntries(BepInEx.Paths.PluginPath, "*", SearchOption.AllDirectories)));
+
+                foreach (var dir in Directory.GetDirectories(pluginRoot))
                 {
-                    DeserializeCustomTextLine(line,returnDict);
+                    var textModderFile = Path.Combine(dir, "TextModder");
+                    if (!File.Exists(textModderFile)) continue;
+                    
+                    TextModderLogger.LogDebug($"Found TextModder Directory: {dir}");
+
+                    foreach (var file in Directory.GetFiles(dir, "*.txt", SearchOption.AllDirectories))
+                    {
+                        TextModderLogger.LogInfo($"Reading Text From File: {file}");
+                        foreach (var line in File.ReadAllLines(file))
+                            DeserializeCustomTextLine(line, returnDict);
+                    }
                 }
             }
-            
+            else
+            {
+                TextModderLogger.LogError("No BepInEx plugin directory found.");
+            }
+
+            // Now read from the main ModDir (overrides)
+            foreach (var filePath in Directory.GetFiles(ModDir))
+                {
+                    TextModderLogger.LogInfo($"Reading Text From File: {filePath}");
+                    foreach (var line in File.ReadAllLines(filePath))
+                    {
+                        DeserializeCustomTextLine(line, returnDict);
+                    }
+                }
+
             return returnDict;
         }
 
         static void DeserializeCustomTextLine(string lineToDeserialize, Dictionary<LanguageCode, Dictionary<string, Dictionary<string, string>>> dictToAddTo)
         {
-            TextModderLogger.LogInfo(lineToDeserialize);
+            TextModderLogger.LogDebug(lineToDeserialize);
 
             if (string.IsNullOrWhiteSpace(lineToDeserialize)) return;
 
@@ -133,14 +160,14 @@ public class TextModderPlugin : BaseUnityPlugin
             var parts = lineToDeserialize.Split(new[] { '>' }, 4); // limit to 4 parts in case the value contains colons
             if (parts.Length != 4)
             {
-                TextModderLogger.LogInfo($"Invalid format: {lineToDeserialize}");
+                TextModderLogger.LogWarning($"Invalid format: {lineToDeserialize}");
                 return;
             }
 
             // Parse language code
             if (!Enum.TryParse(parts[0], out LanguageCode langCode))
             {
-                TextModderLogger.LogInfo($"Unknown language code: {parts[0]}");
+                TextModderLogger.LogWarning($"Unknown language code: {parts[0]}");
                 return;
             }
 
